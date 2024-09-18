6b35c6d10059384b21f6a5e0940d27ed3675f26c
Aibek-Kozhonberdiev <ratroniii@gmail.com>
2024-09-19 00:25:38 +0600

    users model corrected

--- backend/src/apps/users/models.py
+++ backend/src/apps/users/models.py
@@ -1,12 +1,20 @@
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import FileExtensionValidator
 from django.db import models
+from django.utils.translation import gettext as _
 
 from ..base.services import path_avatar_user
 from ..base.validators import validation_phone, validate_file_size_avatar
 
 
 class CustomUser(AbstractUser):
+    email = models.EmailField(
+        _('email address'),
+        unique=True,
+        error_messages={
+            'unique': _("Пользователь с таким адресом электронной почты уже существует."),
+        }
+    )
     phone = models.CharField(
         max_length=16,
         verbose_name='Телефонный номер',
@@ -14,7 +22,7 @@ class CustomUser(AbstractUser):
         validators=[
             validation_phone
         ],
-        unique=True
+        unique=True,
     )
     avatar = models.ImageField(
         null=True,
@@ -33,7 +41,9 @@ class CustomUser(AbstractUser):
         ]
     )
 
-    REQUIRED_FIELDS = ['phone', 'email']
+    REQUIRED_FIELDS = ['email', 'phone']
 
     class Meta:
         app_label = 'users'
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
