97b0522b009f239825b726e80e62b4b1c28257c0
Aibek-Kozhonberdiev <ratroniii@gmail.com>
2024-10-18 14:39:43 +0600

    update wsgi-entrypoint

--- backend/wsgi-entrypoint.sh
+++ backend/wsgi-entrypoint.sh
@@ -3,4 +3,5 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(username='root', email='ratroniii@gmail.com', phone='+996500162325', password='admin')"
 python manage.py runserver 0.0.0.0:8000
