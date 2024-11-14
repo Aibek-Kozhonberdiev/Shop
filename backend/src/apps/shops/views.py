32147d3a83d5db162f40454cb4dcdd59d1fa87ce
Aibek-Kozhonberdiev <ratroniii@gmail.com>
2024-11-15 03:24:10 +0600

    add filter shop app

--- backend/src/apps/shops/views.py
+++ backend/src/apps/shops/views.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
-from  rest_framework import permissions
+from rest_framework import permissions, filters
+from django_filters import rest_framework
 
 from .models import Shop
 from .serializers import ShopSerializer
@@ -9,3 +10,5 @@ class ShopSetView(ModelViewSet):
     queryset = Shop.objects.all()
     serializer_class = ShopSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, ]
+    filter_backends = [filters.SearchFilter, rest_framework.DjangoFilterBackend]
+    search_fields = ['title', 'rating', 'create_to']
